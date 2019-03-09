45a0aa77f926f1557d018cf7675fbd8206825b4e
yashasvi13 <yashasvij13@gmail.com>
2019-03-09 19:21:08 +0530

    poko-times

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Rainbow from "../hoc/Rainbow";
+const About = () => {
+  return (
+    <div className="containter">
+      <h4 className="center">About</h4>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod non ipsam
+        recusandae ipsum. Facilis, deserunt soluta at aut exercitationem, odio
+        eius ipsam labore fugit sequi officia voluptatum assumenda quisquam
+        illum.
+      </p>
+    </div>
+  );
+};
+export default Rainbow(About);
---
+++ src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Contact = props => {
+  // setTimeout(() => {
+  //   props.history.push("./about");
+  // }, 2000);
+
+  return (
+    <div className="containter">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod non ipsam
+        recusandae ipsum. Facilis, deserunt soluta at aut exercitationem, odio
+        eius ipsam labore fugit sequi officia voluptatum assumenda quisquam
+        illum.
+      </p>
+    </div>
+  );
+};
+export default Contact;
